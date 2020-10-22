b52195eabd926056355242e0a1587a1468cb521c
azizzharr <ivanhhd872@gmail.com>
2020-10-22 14:47:18 +0600

    'few'

---
+++ src/component/carousel/carousel.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+class Carousel extends Component {
+    render() {
+        return (
+            <div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://avatars.mds.yandex.net/get-pdb/1676693/aca93e81-3e8c-4570-81ad-04aa07b00b57/s1200?webp=false"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://trikky.ru/wp-content/blogs.dir/1/files/2019/08/30/s1200-3.jpg"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://i.pinimg.com/736x/40/22/4f/40224f4f6d46a10a301114db0cc01684--fancy-houses-hollywood-homes.jpg"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        );
+    }
+}
+
+export default Carousel;
--- src/header/header.js
+++ src/header/header.js
@@ -1,43 +1,47 @@
 import React, {Component} from 'react';
+import header from './header.css'
 
 class Header extends Component {
     render() {
         return (
             <div>
+                    <header className="Haupt">
+                        <nav className="nav">
+                            <ul>
+                                <div className="ml">
+                                </div>
+                                <li>
+                                    О нас
+                                </li>
+                                <li>
+                                    Объекты
+                                </li>
+                                <li>
+                                    Новости
+                                </li>
+                                <li>
+                                    Клиентам
+                                </li>
+                                <li>
+                                    Оплата
+                                </li>
+                                <li>
+                                    Коммерция
+                                </li>
+                                <li>
 
-
-                <header>
-                    <nav>
-                        <ul>
-                            <li>
-                                О нас
-                            </li>
-                            <li>
-                                Объекты
-                            </li>
-                            <li>
-                                Новости
-                            </li>
-                            <li>
-                                Клиентам
-                            </li>
-                            <li>
-                                Оплата
-                            </li>
-                            <li>
-                                Коммерция
-                            </li>
-                            <li>
-
-                                Контакты  | 0 (555) 555-555
-                            </li>
-                        </ul>
-                        <div>
-                            <img src="https://static.tildacdn.com/tild3461-3563-4663-b766-663864623139/-/resize/504x/elite-house-logo.png" alt="logo Elite-House"/>
-                        </div>
-                    </nav>
-                </header>
+                                    Контакты | 0 (555) 555-555
+                                </li>
+                            </ul>
+                            <div>
+                                <img className="img"
+                                     src="https://static.tildacdn.com/tild3461-3563-4663-b766-663864623139/-/resize/504x/elite-house-logo.png"
+                                     alt="logo Elite-House"/>
+                            </div>
+                        </nav>
+                    </header>
             </div>
+
         );
     }
 }
